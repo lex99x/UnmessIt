22c05c886608a3fb33514903f135a22d4f3cecc8
Joao Lucas Camilo <jlf.camilo@icloud.com>
2023-07-24 08:42:10 -0400

    chore: create base projetc.

---
+++ Chores/Chores/ChoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoresApp.swift
+//  Chores
+//
+//  Created by Joao Lucas Camilo on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
