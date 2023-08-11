f8bcc30e93e3695c667828eeaa57bc497750fadb
Alex A. Rocha <lex99r@gmail.com>
2023-08-11 08:53:47 -0400

    chore: add custom fonts to xcode project

---
+++ Chores/Chores/Extension/Font+CustomFonts.swift
@@ -0,0 +1,14 @@
+//
+//  Font+customFont.swift
+//  Chores
+//
+//  Created by Alex A. Rocha on 11/08/23.
+//
+
+import SwiftUI
+
+extension Font {
+    static let generalSansFontRegular = "GeneralSans-Regular"
+    static let generalSansFontMedium = "GeneralSans-Medium"
+    static let generalSansFontSemibold = "GeneralSans-Semibold"
+}
