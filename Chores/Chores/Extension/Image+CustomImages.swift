0948f0f2f8061c017669322d09a708dcfe2cc8ed
Alex A. Rocha <lex99r@gmail.com>
2023-08-16 11:47:50 -0400

    chore: adding custom images extension

---
+++ Chores/Chores/Extension/Image+CustomImages.swift
@@ -0,0 +1,52 @@
+//
+//  Image+CustomImages.swift
+//  Chores
+//
+//  Created by Alex A. Rocha on 16/08/23.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    // MARK: Categories
+    static let catIcon = Image("Cat")
+    static let clothesIcon = Image("Clothes")
+    static let cookingIcon = Image("Cooking")
+    static let dogIcon = Image("Dog")
+    static let heavyCleaningIcon = Image("HeavyCleaning")
+    static let lightCleaningIcon = Image("LightCleaning")
+    static let organizationIcon = Image("Organization")
+    static let paymentsIcon = Image("Payments")
+    static let petsIcon = Image("Pets")
+    static let shoppingIcon = Image("Shopping")
+ 
+    // MARK: General
+    static let arrowLeftIcon = Image("ArrowLeft")
+    static let arrowRightIcon = Image("ArrowRight")
+    static let checkIcon = Image("Check")
+    static let chevronDownIcon = Image("ChevronDown")
+    static let copyIcon = Image("Copy")
+    static let deleteIcon = Image("Delete")
+    static let editIcon = Image("Edit")
+    static let leaveIcon = Image("Leave")
+    static let moreIcon = Image("More")
+    static let pinIcon = Image("Pin")
+    static let placeIcon = Image("Place")
+    static let plusIcon = Image("Plus")
+    static let popupButtonIcon = Image("PopupButton")
+    static let repeatIcon = Image("Repeat")
+    static let searchIcon = Image("Search")
+    static let shareIcon = Image("Share")
+    static let tasksIcon = Image("Tasks")
+    static let userAddIcon = Image("UserAdd")
+    static let userRemoveIcon = Image("UserRemove")
+    static let userRotationIcon = Image("UserRotation")
+    static let xcircleIcon = Image("Xcircle")
+    
+    // MARK: Status
+    static let blockedStatusIcon = Image("StatusBlock")
+    static let doneStatusIcon = Image("StatusDone")
+    static let pendingStatusIcon = Image("StatusPending")
+    
+}
