bd9bbb0ccad53e5bad87e8ae9d32d2cea230be67
Joao Lucas Camilo <jlf.camilo@icloud.com>
2023-09-11 22:40:55 -0400

    feat: edit residents fixed.

--- Chores/Chores/View/Residents/NewResident.swift
+++ Chores/Chores/View/Residents/NewResident.swift
@@ -12,40 +12,83 @@ struct NewResident: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject private var newResidentViewModel = NewResidentViewModel()
     let isEditing: Bool
-    
+    let isSpaceOwner: Bool
+    @State private var isShowingDeleteAlert = false
     @ObservedRealmObject var resident: User
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            
-            Text("Name")
-                .font(.title3)
-            
-            ResidentInputView(residentName: $newResidentViewModel.residentName)
-            
-            
-            
-            Text("Prefered Tasks")
-                .font(.title3)
-            
-            List {
-                ForEach(TaskCategory.values, id: \.self) { item in
-                    MultipleSelectionRow(title: item.rawValue, isSelected: newResidentViewModel.selections.contains(item.rawValue)) {
-                        if newResidentViewModel.selections.contains(item.rawValue) {
-                            newResidentViewModel.selections.removeAll(where: { $0 == item.rawValue })
+        VStack {
+            VStack(alignment: .leading, spacing: 12) {
+                
+                Text("resident_input_name_title".localized)
+                    .font(.title3)
+                
+                ResidentInputView(residentName: $newResidentViewModel.residentName)
+                
+                Text("preferences_title".localized)
+                    .font(.title3)
+                
+                ZStack {
+                    ScrollView {
+                        ForEach(TaskCategory.values, id: \.self) { item in
+                            MultipleSelectionRow(title: item.rawValue, isSelected: newResidentViewModel.selections.contains(item.rawValue)) {
+                                if newResidentViewModel.selections.contains(item.rawValue) {
+                                    newResidentViewModel.selections.removeAll(where: { $0 == item.rawValue })
+                                }
+                                else {
+                                    newResidentViewModel.selections.append(item.rawValue)
+                                }
+                            }
+                            if item.rawValue != TaskCategory.values.last?.rawValue {
+                                Divider()
+                            }
                         }
-                        else {
-                            newResidentViewModel.selections.append(item.rawValue)
-                        }
+                        
+                        .overlay(
+                          RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.borderDefaultColor, lineWidth: 3)
+
+                            
+                        )
+//                        .foregroundColor(Color.accent2)
+//                        .background(Color(hex: "303030"))
+                        .background(Color(hex: "303030"))
+                        .cornerRadius(10)
+                    }
+//                    .foregroundColor(Color.accent2)
+                    
+                }
+                
+                
+            }
+            
+            if isEditing == true && isSpaceOwner == false {
+                Spacer()
+                Button {
+                    isShowingDeleteAlert.toggle()
+                } label: {
+                    HStack {
+                        Image.userRemoveIcon
+                        Text("alert_delete_resident_title".localized)
                         
                     }
                 }
-                .listRowSeparator(.hidden)
-                .listRowInsets(EdgeInsets())
+                .buttonStyle(CustomButtonStyle(width: .infinity,
+                                               foregroundColor: .surface_surfaceTetriary,
+                                               backgroundColor: .text_criticalText))
             }
-            .listStyle(.inset)
-            
         }
+        .alert("alert_delete_resident_title".localized, isPresented: $isShowingDeleteAlert, actions: {
+            Button("alert_delete_resident_tasks_action_left".localized, role: .cancel) {
+                isShowingDeleteAlert.toggle()
+            }
+            Button("alert_delete_resident_tasks_action_right".localized, role: .destructive) {
+                newResidentViewModel.deleteResident(item: resident)
+                dismiss()
+            }
+        }, message: {
+            Text("alert_delete_resident_description".localized)
+        })
         .alert("alert_resident_missing_fields_title".localized, isPresented: $newResidentViewModel.hasError, actions: {
 //                    Button("Cancel", role: .cancel) {
 //                        isShowingDeleteAlert.toggle()
@@ -59,14 +102,14 @@ struct NewResident: View {
         
         .padding()
         .navigationBarBackButtonHidden(true)
-        .navigationTitle(isEditing ? "Edit Resident" : "New Resident")
+        .navigationTitle(isEditing ? "edit_resident_title".localized : "new_resident_title".localized)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Button(action: {
                     dismiss()
                 }, label: {
-                    Text("Cancel")
+                    Text("resident_button_cancel".localized)
                 })
             }
             ToolbarItem(placement: .navigationBarTrailing) {
@@ -82,7 +125,7 @@ struct NewResident: View {
                         dismiss()
                     }
                 }, label: {
-                    Text("Save")
+                    Text("resident_button_save".localized)
                 })
             }
             
@@ -102,25 +145,25 @@ struct NewResident: View {
 
 }
 
-
-struct NewResident_Previews: PreviewProvider {
-    static var previews: some View {
-    
-            Group {
-//                NavigationView {
-                NewResident(isEditing: false, resident: User())
-                        .previewDisplayName("New User")
-                    
-                    let user = User(value: ["nickname":"joaozinho", "preferences":["Cooking"]])
-                    NewResident(isEditing: true, resident: user)
-                        .previewDisplayName("Filled")
-
-//                }
-            }
-            
-                
-        }
-    }
+//
+//struct NewResident_Previews: PreviewProvider {
+//    static var previews: some View {
+//    
+//            Group {
+////                NavigationView {
+//                NewResident(isEditing: false, isSpaceOwner: false, resident: User())
+//                        .previewDisplayName("New User")
+//                    
+//                    let user = User(value: ["nickname":"joaozinho", "preferences":["Cooking"]])
+//                    NewResident(isEditing: true, resident: user)
+//                        .previewDisplayName("Filled")
+//
+////                }
+//            }
+//            
+//                
+//        }
+//    }
 
 
 struct MultipleSelectionRow: View {
@@ -129,24 +172,30 @@ struct MultipleSelectionRow: View {
     var action: () -> Void
 
     var body: some View {
-        
-        Button(action: self.action) {
-            HStack {
-                if self.isSelected {
-                    Image(systemName: "checkmark")
+        ZStack{
+            Button(action: self.action) {
+                HStack {
+                    if self.isSelected {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(Color(hex: "F5F5F5"))
+                    }
+                    Text(self.title.localized)
+                        .foregroundColor(Color(hex: "F5F5F5"))
+    //                    .padding([.leading],50)
+                    Spacer()
+                    Image(self.title)
+                        .renderingMode(.original)
                 }
-                Text(self.title)
-//                    .padding([.leading],50)
-                Spacer()
-                Image(self.title)
+                
             }
-            
         }
         .padding()
-        .overlay(
-          RoundedRectangle(cornerRadius: 5)
-            .stroke(Color.borderDefaultColor, lineWidth: 1)
-        )
+//        .background(Color(hex: "303030"))
+
+//        .overlay(
+//          RoundedRectangle(cornerRadius: 5)
+//            .stroke(Color.borderDefaultColor, lineWidth: 3)
+//        )
     }
 }
 
