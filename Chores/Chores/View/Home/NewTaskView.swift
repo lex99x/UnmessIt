df9a7f2e98f907d34f2d1f35bf9ed9e519416465
Joao Lucas Camilo <jlf.camilo@icloud.com>
2023-09-20 11:31:19 -0400

    fix: dismiss on touch out of keyboard,add task scroll.

--- Chores/Chores/View/Home/NewTaskView.swift
+++ Chores/Chores/View/Home/NewTaskView.swift
@@ -20,156 +20,161 @@ struct NewTaskView: View {
     var body: some View {
         
         VStack {
-            
-            Divider()
-            
-            VStack(alignment: .leading, spacing: 24) {
+            ScrollView {
+                Divider()
                 
-                VStack(alignment: .leading, spacing: 8) {
+                VStack(alignment: .leading, spacing: 24) {
                     
-                    Text("task_input_type_title")
-                        .font(Font.custom(Font.generalSansFontMedium, size: 15))
-                        .foregroundColor(.textPrimaryColor)
-                    
-                    CustomSelectionInputView(placeholder: isEditing ?  task.category.rawValue : "task_input_type_placeholder".localized,
-                                             options: Task.taskOptions,
-                                             selectedOption: $viewModel.selectedTaskTypeOption)
-                    .onChange(of: viewModel.selectedTaskTypeOption) { newValue in
+                    VStack(alignment: .leading, spacing: 8) {
                         
-                        if let recommendation = viewModel.recommendAssigneer(item: newValue) {
-                            viewModel.selectedAssigneeOption = recommendation
-                        } else {
-                            viewModel.selectedAssigneeOption = User()
+                        Text("task_input_type_title")
+                            .font(Font.custom(Font.generalSansFontMedium, size: 15))
+                            .foregroundColor(.textPrimaryColor)
+                        
+                        CustomSelectionInputView(placeholder: isEditing ?  task.category.rawValue : "task_input_type_placeholder".localized,
+                                                 options: Task.taskOptions,
+                                                 selectedOption: $viewModel.selectedTaskTypeOption)
+                        .onChange(of: viewModel.selectedTaskTypeOption) { newValue in
+                            
+                            if let recommendation = viewModel.recommendAssigneer(item: newValue) {
+                                viewModel.selectedAssigneeOption = recommendation
+                            } else {
+                                viewModel.selectedAssigneeOption = User()
+                            }
+                            
                         }
                         
                     }
                     
-                }
-                
-                CustomTextFieldView(title: "task_input_name_title".localized,
-                                    placeholder: "task_input_name_placeholder".localized,
-                                    isOptional: false,
-                                    textfield: $viewModel.titleTextfield)
-                
-                CustomTextFieldView(title: "task_input_description_title".localized,
-                                    placeholder: "task_input_description_placeholder".localized,
-                                    isOptional: true,
-                                    textfield: $viewModel.descriptionTextfield)
-                
-                VStack(alignment: .leading, spacing: 8) {
+                    CustomTextFieldView(title: "task_input_name_title".localized,
+                                        placeholder: "task_input_name_placeholder".localized,
+                                        isOptional: false,
+                                        textfield: $viewModel.titleTextfield)
                     
-                    Text("task_input_when_title")
-                        .font(Font.custom(Font.generalSansFontMedium, size: 15))
-                        .foregroundColor(.textPrimaryColor)
+                    CustomTextFieldView(title: "task_input_description_title".localized,
+                                        placeholder: "task_input_description_placeholder".localized,
+                                        isOptional: true,
+                                        textfield: $viewModel.descriptionTextfield)
                     
-                    VStack(alignment: .leading, spacing: 6){
-                        DatePicker("task_input_when_date", selection: $viewModel.selectedDate, displayedComponents: .date)
-                        Divider()
-                        DatePicker("task_input_when_time", selection: $viewModel.selectedDate , displayedComponents: .hourAndMinute)
-                    }
-                    .font(Font.custom(Font.generalSansFontRegular, size: 15))
-                    .foregroundColor(.textPrimaryColor)
-                    .padding(.vertical, 6)
-                    .padding(.horizontal, 12)
-                    .inputOverlay()
-                    
-                }
-                
-                VStack(alignment: .leading) {
-                    
-                    Text("task_assignee_title")
-                        .font(Font.custom(Font.generalSansFontMedium, size: 15))
-                        .foregroundColor(.textPrimaryColor)
+                    VStack(alignment: .leading, spacing: 8) {
                         
-                    AssigneesInputView(placeholder: task.assignees.first?.localizedNickname() ?? "task_assignee_placeholder".localized,
-                                       assignees: viewModel.selectedSpace!.residents,
-                                       selectedAssignee: $viewModel.selectedAssigneeOption)
-                    
-                }
-                
-                .alert("alert_task_missing_fields_title".localized, isPresented: $viewModel.hasError, actions: {
-                    Button("alert_task_missing_fields_action".localized, role: .cancel) {
-                        viewModel.hasError = false
-                    }
-                }, message: {
-                    Text("alert_task_missing_fields_description")
-                })
-                
-                // delete task alert
-                .alert("alert_delete_task_title".localized, isPresented: $isShowingDeleteAlert, actions: {
-                    Button("alert_delete_task_action_left".localized, role: .cancel) {
-                        isShowingDeleteAlert.toggle()
-                    }
-                    Button("alert_delete_task_action_right".localized, role: .destructive) {
-                        viewModel.deleteTask(item: task)
-                    }
-                }, message: {
-                    Text("alert_delete_task_description".localized)
-                })
-                
-                Spacer()
-                
-                if isEditing {
-                    Button(action: {
-                        isShowingDeleteAlert.toggle()
-                    }, label: {
-                        HStack {
-                            Image.deleteIcon
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                            Text("edit_task_button_delete")
-                                .font(Font.custom(Font.generalSansFontRegular, size: 15))
-                                .fontWeight(.medium)
+                        Text("task_input_when_title")
+                            .font(Font.custom(Font.generalSansFontMedium, size: 15))
+                            .foregroundColor(.textPrimaryColor)
+                        
+                        VStack(alignment: .leading, spacing: 6){
+                            DatePicker("task_input_when_date", selection: $viewModel.selectedDate, displayedComponents: .date)
+                            Divider()
+                            DatePicker("task_input_when_time", selection: $viewModel.selectedDate , displayedComponents: .hourAndMinute)
                         }
-                        .foregroundColor(.textCriticalColor)
+                        .font(Font.custom(Font.generalSansFontRegular, size: 15))
+                        .foregroundColor(.textPrimaryColor)
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 12)
+                        .inputOverlay()
+                        
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text("task_assignee_title")
+                            .font(Font.custom(Font.generalSansFontMedium, size: 15))
+                            .foregroundColor(.textPrimaryColor)
+                        
+                        AssigneesInputView(placeholder: task.assignees.first?.localizedNickname() ?? "task_assignee_placeholder".localized,
+                                           assignees: viewModel.selectedSpace!.residents,
+                                           selectedAssignee: $viewModel.selectedAssigneeOption)
+                        
+                    }
+                    
+                    .alert("alert_task_missing_fields_title".localized, isPresented: $viewModel.hasError, actions: {
+                        Button("alert_task_missing_fields_action".localized, role: .cancel) {
+                            viewModel.hasError = false
+                        }
+                    }, message: {
+                        Text("alert_task_missing_fields_description")
                     })
-                    .buttonStyle(CustomButtonStyle(width: .infinity,
-                                                   foregroundColor: nil,
-                                                   backgroundColor: .surfaceTertiaryColor))
-                }
-                
-            }
-            .padding(.top, 26)
-            .padding(.horizontal)
-            .navigationBarBackButtonHidden()
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        dismiss()
-                    }, label: {
-                        Text("task_button_cancel")
-                            .font(Font.custom(Font.generalSansFontRegular, size: 17))
-                            .foregroundColor(.textAccentColor)
-                    })
-                }
-                
-                ToolbarItem(placement: .principal) {
-                    Text(isEditing ? "edit_task_title" : "new_task_title")
-                        .font(Font.custom(Font.generalSansFontRegular, size: 17))
-                        .foregroundColor(.textPrimaryColor)
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        if isEditing {
-                            viewModel.updateTask(item: task)
-                        } else {
-                            viewModel.addNewTask()
+                    
+                    // delete task alert
+                    .alert("alert_delete_task_title".localized, isPresented: $isShowingDeleteAlert, actions: {
+                        Button("alert_delete_task_action_left".localized, role: .cancel) {
+                            isShowingDeleteAlert.toggle()
                         }
-                        if viewModel.hasError == false {
+                        Button("alert_delete_task_action_right".localized, role: .destructive) {
+                            viewModel.deleteTask(item: task)
+                        }
+                    }, message: {
+                        Text("alert_delete_task_description".localized)
+                    })
+                    
+                    Spacer()
+                    
+                    if isEditing {
+                        Button(action: {
+                            isShowingDeleteAlert.toggle()
+                        }, label: {
+                            HStack {
+                                Image.deleteIcon
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("edit_task_button_delete")
+                                    .font(Font.custom(Font.generalSansFontRegular, size: 15))
+                                    .fontWeight(.medium)
+                            }
+                            .foregroundColor(.textCriticalColor)
+                        })
+                        .buttonStyle(CustomButtonStyle(width: .infinity,
+                                                       foregroundColor: nil,
+                                                       backgroundColor: .surfaceTertiaryColor))
+                    }
+                    
+                }
+                .padding(.top, 26)
+                .padding(.horizontal)
+                .navigationBarBackButtonHidden()
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
                             dismiss()
-                        }
-                    }, label: {
-                        Text("task_button_save")
+                        }, label: {
+                            Text("task_button_cancel")
+                                .font(Font.custom(Font.generalSansFontRegular, size: 17))
+                                .foregroundColor(.textAccentColor)
+                        })
+                    }
+                    
+                    ToolbarItem(placement: .principal) {
+                        Text(isEditing ? "edit_task_title" : "new_task_title")
                             .font(Font.custom(Font.generalSansFontRegular, size: 17))
-                            .foregroundColor(.textAccentColor)
-                    })
+                            .foregroundColor(.textPrimaryColor)
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            if isEditing {
+                                viewModel.updateTask(item: task)
+                            } else {
+                                viewModel.addNewTask()
+                            }
+                            if viewModel.hasError == false {
+                                dismiss()
+                            }
+                        }, label: {
+                            Text("task_button_save")
+                                .font(Font.custom(Font.generalSansFontRegular, size: 17))
+                                .foregroundColor(.textAccentColor)
+                        })
+                    }
+                    
                 }
                 
             }
-            
+        }
+        .ignoresSafeArea(.keyboard)
+        .onTapGesture {
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
         .background {
             Color.surfaceSheetColor
