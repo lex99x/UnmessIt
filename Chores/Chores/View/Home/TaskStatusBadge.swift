f3a101fe590a8107277479efa0d75b162bd6531d
Alex A. Rocha <lex99r@gmail.com>
2023-09-25 10:24:17 -0400

    fix: fixing colors extension

--- Chores/Chores/View/Home/TaskStatusBadge.swift
+++ Chores/Chores/View/Home/TaskStatusBadge.swift
@@ -9,42 +9,50 @@ import SwiftUI
 
 struct TaskStatusBadge: View {
     
-    @State var status: Task.Status
+    let status: Task.Status
     
-    let colors: (Color, Color)
-    let content: (icon: Image, text: String)
+    let statusIcon: Image
+    let statusText: String
+    let contentColor: Color
+    let surfaceColor: Color
     
     init(status: Task.Status) {
         self.status = status
         switch status {
             case .pending:
-                self.colors = (.badgeTextPendingColor, .badgeSurfacePendingColor)
-                self.content = (Image.pendingStatusIcon, "home_task_status_pending".localized)
+                statusIcon = Image.pendingStatusIcon
+                statusText = "home_task_status_pending".localized
+                contentColor = .badgeTextPendingColor
+                surfaceColor = .badgeSurfacePendingColor
             case .done:
-                self.colors = (.doneTaskBadgeText, .doneTaskBadgeBackground)
-                self.content = (Image.doneStatusIcon, "home_task_status_done".localized)
+                statusIcon = Image.doneStatusIcon
+                statusText = "home_task_status_done".localized
+                contentColor = .badgeTextDoneColor
+                surfaceColor = .badgeSurfaceDoneColor
             case .cantDo:
-                self.colors = (.notDoneTaskBadgeText, .notDoneTaskBadgeBackground)
-                self.content = (Image.blockedStatusIcon, "Not done")
+                statusIcon = Image.blockedStatusIcon
+                statusText = "Not done"
+                contentColor = .badgeTextBlockedColor
+                surfaceColor = .badgeSurfaceBlockedColor
         }
     }
     
     var body: some View {
         ZStack {
             HStack {
-                self.content.0
+                statusIcon
                     .resizable()
                     .frame(width: 16, height: 16)
-                    .foregroundColor(self.colors.0)
-                Text(self.content.1)
-                    .foregroundColor(self.colors.0)
+                    .foregroundColor(contentColor)
+                Text(statusText)
+                    .foregroundColor(contentColor)
                     .fixedSize()
             }
             .font(Font.custom(Font.generalSansFontRegular, size: 15))
             .padding(.leading, 8)
             .padding(.trailing, 12)
             .padding(.vertical, 4)
-            .background(self.colors.1)
+            .background(surfaceColor)
             .cornerRadius(16)
         }
     }
@@ -55,5 +63,6 @@ struct TaskStatusBadge_Previews: PreviewProvider {
     static var previews: some View {
         TaskStatusBadge(status: .done)
         TaskStatusBadge(status: .pending)
+        TaskStatusBadge(status: .cantDo)
     }
 }
